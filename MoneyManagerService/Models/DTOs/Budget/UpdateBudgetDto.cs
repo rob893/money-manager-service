9d282a7cbd24371e8917d0f24ab92a4eb7b66ff7
Robert Herber <rwherber@gmail.com>
2020-12-30 17:11:36 -0800

    feat: add name to update budget endpoint

--- MoneyManagerService/Models/DTOs/Budget/UpdateBudgetDto.cs
+++ MoneyManagerService/Models/DTOs/Budget/UpdateBudgetDto.cs
@@ -2,6 +2,7 @@ namespace MoneyManagerService.Models.DTOs.Budget
 {
     public record UpdateBudgetDto
     {
+        public string? Name { get; init; }
         public string? Description { get; init; }
     }
 }
